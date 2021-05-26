43d0e29b18ffafcb6db90cd2d1266a576d85f33b
XCoreX <xcorex77@gmail.com>
2021-05-26 15:34:47 +0300

    First commit

---
+++ src/main/java/group/bibliopole/BibliopoleApplication.java
@@ -0,0 +1,13 @@
+package group.bibliopole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliopoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliopoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/bibliopole/BibliopoleApplicationTests.java
@@ -0,0 +1,13 @@
+package group.bibliopole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliopoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
