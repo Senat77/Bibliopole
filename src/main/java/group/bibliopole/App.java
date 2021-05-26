da174eab3442d75d03277429cd9397e818123db2
XCoreX <xcorex77@gmail.com>
2021-05-26 16:31:36 +0300

    First commit

--- src/main/java/group/bibliopole/App.java
+++ src/main/java/group/bibliopole/App.java
@@ -1,7 +1,13 @@
 package group.bibliopole;
 
+import group.bibliopole.model.Book;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class App {
@@ -10,4 +16,13 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
+    @Profile("h2")
+    @Bean
+    public CommandLineRunner demo(BookRepository repository) {
+        return (args) -> {
+            repository.saveAll(Arrays.asList(
+                    new Book("Материнская плата", true, 3L)
+            ));
+        };
+    }
 }
--- src/main/java/group/bibliopole/model/Book.java
+++ src/main/java/group/bibliopole/model/Book.java
@@ -1,15 +1,31 @@
 package group.bibliopole.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "books")
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String author;
+
+    private String publishing;
+
+    private int year;
+
+    private int pages;
+
+    private float cost;
 }
